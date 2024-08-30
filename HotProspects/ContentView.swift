e525b9f9ff726fd0b58c1c1977f35e554c80294b
cosmingradinariu <126178291+cosmingradinariu@users.noreply.github.com>
2024-08-30 12:34:28 +0300

    Initial Commit

---
+++ HotProspects/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by Cosmin Gradinariu on 29.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            ProspectsView(filter: .none)
+                .tabItem {
+                    Label("Everyone", systemImage: "person.3")
+                }
+            
+            ProspectsView(filter: .contacted)
+                .tabItem {
+                    Label("Contacted", systemImage: "checkmark.circle.fill")
+                }
+            
+            ProspectsView(filter: .uncontacted)
+                .tabItem {
+                    Label("Uncontacted", systemImage: "questionmark.diamond")
+                }
+            
+            MeView()
+                .tabItem {
+                    Label("Me", systemImage: "person.crop.square")
+                }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
